0da8e0487ed41a646f280c6cdf66df0b60b56760
nishio-kun <soreil310lune@gmail.com>
2020-04-17 12:30:09 +0900

    Add function: mask

--- src/main.py
+++ src/main.py
@@ -39,6 +39,19 @@ def show_image_array(img):
     plt.show()
 
 
+def mask(base_img, mask_img, threshold=5):
+    """
+    Mask image.
+    """
+
+    mask_img_gray = cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(mask_img_gray, threshold, 255, cv2.THRESH_BINARY)
+
+    ret_img = cv2.bitwise_and(base_img, base_img, mask=mask)
+
+    return ret_img
+
+
 def save_image(path, img):
     """
     Save image.
@@ -53,9 +66,10 @@ def main():
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('cmd', choices=['show', 'change'],
-                        help='"cmd" is "show" or "change".')
+    parser.add_argument('cmd', choices=['show', 'change', 'mask'],
+                        help='"cmd" is "show", "change" or "mask".')
     parser.add_argument('img_path', help='Path of the target image.')
+    parser.add_argument('-m', help='Path of the mask image.')
     parser.add_argument('-f', type=int, choices=range(0, 256),
                         help='Change color from that.')
     parser.add_argument('-t', type=int, choices=range(0, 256),
@@ -65,6 +79,7 @@ def main():
 
     cmd = args.cmd
     img_path = args.img_path
+    mask_img_path = args.m
     from_ = args.f
     to = args.t
     save_img_path = args.save
@@ -77,12 +92,27 @@ def main():
         if (from_ is None) or (to is None):
             print('"-f", "-t" are required if "cmd" is "change"')
             sys.exit(1)
-        change_color(img, from_, to)
-        show_image(img)
 
-    if save_img_path:
-        save_image(save_img_path, img)
-        print(f'saved to {save_img_path}')
+        change_color(img, from_, to)
+
+        if save_img_path:
+            save_image(save_img_path, img)
+            print(f'saved to {save_img_path}')
+        else:
+            show_image(img)
+    elif cmd == 'mask':
+        if not mask_img_path:
+            print('"-m" is required if "cmd" is "mask"')
+            sys.exit(1)
+
+        mask_img = cv2.imread(mask_img_path)
+        masked_img = mask(img, mask_img)
+
+        if save_img_path:
+            save_image(save_img_path, masked_img)
+            print(f'saved to {save_img_path}')
+        else:
+            show_image(masked_img)
 
 
 if __name__ == '__main__':
